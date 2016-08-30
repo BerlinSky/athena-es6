260ad3539504517e1f917a391f8d0932e7147d8e
Berlin Sky <pedroyoung8989@gmail.com>
2016-08-30 09:06:31 -0700

    Add book table

--- src/scripts/ui-components/search-panel.js
+++ src/scripts/ui-components/search-panel.js
@@ -9,14 +9,117 @@ export class SearchPanel extends UIBaseComponent {
   
   addComponent() {
     return `
-       
-        <div class="formContainer">
-        <form class="searchBar">
-        <input type="text" class="searchBar__inputText" placeholder="Search ..." name="">
-        <input type="submit" class="searchBar__button" value="" name="">
-        </form>
+      <div class="formContainer">
+      <form class="searchBar">
+      <input type="text" class="searchBar__inputText" placeholder="Search ..." name="">
+      <input type="submit" class="searchBar__button" value="" name="">
+      </form>
+      </div>
+
+      <div>
+        <div class="l-grid__container grid__container grid__container--ruler"> 
+          <div class="l-grid__span--xs-16 l-grid__span--md-2">
+            <div class="columnHeading__dataLabel">ISBN</div>
+          </div> 
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="columnHeading__dataLabel">TITLE</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="columnHeading__dataLabel">AUTHOR</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="columnHeading__dataLabel">PUBLISHER</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="columnHeading__dataLabel">PUBLISHED</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-2">
+            <div class="columnHeading__dataLabel columnHeading__dataLabel--numeric">PRICE ($)</div>
+          </div>
         </div>
-      
+
+        <div class="l-grid__container grid__container grid__container--ruler"> 
+          <div class="l-grid__span--xs-16 l-grid__span--md-2">
+            <div class="dataLabel">ISBN</div>
+            <div class="dataValue">0446310786</div>
+          </div> 
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">TITLE</div>
+            <div class="dataValue">To Kill a Mockingbird</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">AUTHOR</div>
+            <div class="dataValue">Haper Lee</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">PUBLISHER</div>
+            <div class="dataValue">Grand Central Publishing</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">PUBLISHED</div>
+            <div class="dataValue">October 11, 1988</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-2">
+            <div class="dataLabel">PRICE ($)</div>
+            <div class="dataValue dataValue--numeric">17.33</div>
+          </div>
+        </div>
+
+        <div class="l-grid__container grid__container grid__container--ruler"> 
+          <div class="l-grid__span--xs-16 l-grid__span--md-2">
+            <div class="dataLabel">ISBN</div>
+            <div class="dataValue">0141439556</div>
+          </div> 
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">TITLE</div>
+            <div class="dataValue">Wuthering Heights</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">AUTHOR</div>
+            <div class="dataValue">Emily Bronte</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">PUBLISHER</div>
+            <div class="dataValue">Penguin Classic</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">PUBLISHED</div>
+            <div class="dataValue">December 31, 2002</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-2">
+            <div class="dataLabel">PRICE ($)</div>
+            <div class="dataValue dataValue--numeric">12.99</div>
+          </div>
+        </div>
+
+        <div class="l-grid__container grid__container grid__container--ruler"> 
+          <div class="l-grid__span--xs-16 l-grid__span--md-2">
+            <div class="dataLabel">ISBN</div>
+            <div class="dataValue">0684801221</div>
+          </div> 
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">TITLE</div>
+            <div class="dataValue">The Old Man and The Sea</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">AUTHOR</div>
+            <div class="dataValue">Ernest Hemingways</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">PUBLISHER</div>
+            <div class="dataValue">Scribner</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-3">
+            <div class="dataLabel">PUBLISHED</div>
+            <div class="dataValue">May 5, 1995</div>
+          </div>
+          <div class="l-grid__span--xs-16 l-grid__span--md-2">
+            <div class="dataLabel">PRICE ($)</div>
+            <div class="dataValue dataValue--numeric">15.28</div>
+          </div>
+        </div>
+
+      </div>
     `;
   }
 
